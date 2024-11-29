48912d31ef2860bce70f69dbffd3ec7b52ee1751
Md. Abu Rayhan Sushmoy <110671422+AR-Sushmoy@users.noreply.github.com>
2024-11-29 19:19:14 +0000

    implemented dynamic data, API route, and some photos

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Dhaka, Bangladesh',
+    'salary': '55,000 BDT'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Chittagong, Bangladesh',
+    'salary': '65,000 BDT'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Sylhet, Bangladesh'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'Remote',
+    'salary': '$2,000'
+  },
+]
+
 @app.route("/")
-def hello_world():
-  return "Hello, Sushmoy"
+def open():
+  return render_template('home.html',
+                         jobs=JOBS,
+                        company_name='Horizon')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)

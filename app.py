2225c83ff4e7b33ab6a2c6ecc864c67cc76c4f77
Md. Abu Rayhan Sushmoy <110671422+AR-Sushmoy@users.noreply.github.com>
2024-11-30 07:56:22 +0000

    made functional improvements to the buttons and added navbar and footer

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ JOBS = [
 ]
 
 @app.route("/")
-def open():
+def home():
   return render_template('home.html',
                          jobs=JOBS,
                         company_name='Horizon')

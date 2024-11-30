40698a6f075e22f06b59a38eb2048c87924ecd83
Md. Abu Rayhan Sushmoy <110671422+AR-Sushmoy@users.noreply.github.com>
2024-11-30 23:00:18 +0000

    skeleton in the closet

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
 from flask import Flask, render_template, jsonify
-from database import engine
-from sqlalchemy import text
+from database import load_jobs_from_db
 
 app = Flask(__name__)
-
-
-def load_jobs_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    jobs = []
-    for row in result.all():
-      jobs.append(row._asdict())
-    return jobs
 
 
 @app.route("/")
@@ -19,6 +9,11 @@ def home():
   JOBS = load_jobs_from_db()
   return render_template('home.html', jobs=JOBS, company_name='Horizon')
 
+
+@app.route("/api/jobs")
+def list_of_jobs():
+  JOBS = load_jobs_from_db()
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
